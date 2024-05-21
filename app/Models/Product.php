bdf6c57bb3dc306ade1e4ca37ab5cd8e27f20d14
mohamed elmegary <168588907+moelmegary@users.noreply.github.com>
2024-05-21 17:43:09 +0300

    Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,7 +24,7 @@ class Product extends Model
          */
         public function categories()
         {
-            return $this->belongsToMany(Category::class);
+            return $this->belongsToMany(Category::class,'products_categories','Product_id','category_id');
         }
     }
 
